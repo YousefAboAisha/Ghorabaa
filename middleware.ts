bea8523752c34074b0a8a45972152d40e61e2a14
YousefAboAisha <yousef.aboesha@hotmail.com>
2025-05-12 16:07:35 +0300

    Activating the middleware logic for deployment

--- middleware.ts
+++ middleware.ts
@@ -2,28 +2,35 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // const token =
-  //   request.cookies.get("next-auth.session-token")?.value ||
-  //   request.cookies.get("__Secure-next-auth.session-token")?.value;
+  const token =
+    request.cookies.get("next-auth.session-token")?.value ||
+    request.cookies.get("__Secure-next-auth.session-token")?.value;
 
-  // const isAuthPage =
-  //   request.nextUrl.pathname.startsWith("/auth") ||
-  //   request.nextUrl.pathname === "/signin" ||
-  //   request.nextUrl.pathname === "/signup";
+  const isAuthPage =
+    request.nextUrl.pathname.startsWith("/auth") ||
+    request.nextUrl.pathname === "/signin" ||
+    request.nextUrl.pathname === "/signup";
 
-  // if (!token && !isAuthPage) {
-  //   // Not logged in, trying to access protected route
-  //   return NextResponse.redirect(new URL("/signin", request.url));
-  // }
+  if (!token && !isAuthPage) {
+    // Not logged in, trying to access protected route
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
 
-  // if (token && isAuthPage) {
-  //   // Already logged in, trying to access login/register
-  //   return NextResponse.redirect(new URL("/profile", request.url));
-  // }
+  if (token && isAuthPage) {
+    // Already logged in, trying to access login/register
+    return NextResponse.redirect(new URL("/profile", request.url));
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/profile/:path*", "/auth/:path*", "/signin", "/signup"],
+  matcher: [
+    "/profile/:path*",
+    "/addStory/:path*",
+    "/editProfile/:path*",
+    "/auth/:path*",
+    "/signin",
+    "/signup",
+  ],
 };
