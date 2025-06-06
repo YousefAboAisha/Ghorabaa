36a269fb754b12af44bb3741d96fd16b193851ce
YousefAboAisha <74072781+YousefAboAisha@users.noreply.github.com>
2025-06-06 17:49:20 +0300

    Fix building issues..

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,12 +1,9 @@
 import SigninForm from "@/components/UI/Forms/signinForm";
-import { getSessionAction } from "../actions/registerActions";
 
 const Signin = async () => {
-  const session = await getSessionAction();
-
   return (
     <div className="relative min-h-screen flex items-center justify-center">
-      <SigninForm session={session} />
+      <SigninForm />
     </div>
   );
 };
